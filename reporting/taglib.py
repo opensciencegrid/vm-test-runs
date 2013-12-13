a2afcb1b467f34674a02478fc2004bd518c161c5
Brian Lin <blin@cs.wisc.edu>
2013-12-13 23:25:51 +0000

    Create basic report pages for VMU tests (SOFTWARE-1304)
    git-svn-id: https://vdt.cs.wisc.edu/svn/new-test/trunk/vm-test-runs@18430 4e558342-562e-0410-864c-e07659590f8c

---
+++ reporting/taglib.py
@@ -0,0 +1,137 @@
+"""A helper class for generating htmls. API vaguely based on ElementTree but
+the output is nicely indented for readability."""
+
+import re
+import types
+
+def indented(string_to_indent, indent):
+    """Return the indented version of a string. 'indent' is the number of spaces
+    to add before each line.
+
+    """
+    indent_str = " " * indent
+    string_as_list = string_to_indent.split("\n")
+    result = indent_str + ("\n" + indent_str).join(string_as_list)
+    return result
+    
+
+class Tag(object):
+    """Holds a tag with a name, attributes and content.
+    content is a list of either tags or strings.
+
+    """
+    def __init__(self, tag_name, attribs=None, **kwattribs):
+        self.contents = []
+        self.tag_name = tag_name
+        if attribs is None:
+            attribs = {}
+        self.attribs = attribs
+        # special case for 'class_': 'class' is used often in the htmls,
+        # but is a python reserved word, so use the value of the kwarg
+        # 'class_' as the value of the attribute 'class'.
+        # same thing for 'type_'
+        for kwattrib_name in ['class', 'type']:
+            kwattrib_name_ = kwattrib_name + "_"
+            if kwattribs.has_key(kwattrib_name_):
+                kwattrib_value = kwattribs.pop(kwattrib_name_)
+                if kwattrib_value is not None:
+                    self.attribs[kwattrib_name] = kwattrib_value
+        self.attribs.update(kwattribs)
+
+    def append(self, item):
+        """Add the item to the contents. Returns self for easy chaining."""
+        self.contents.append(item)
+        return self
+
+    def extend(self, items):
+        """Add each item to the contents. Returns self for easy chaining."""
+        self.contents.extend(items)
+        return self
+
+    def append_escaped(self, text):
+        """Add the value to the contents, with html special characters escaped.
+        Returns self for easy chaining.
+
+        """
+        escaped = re.sub('[&]', '&amp;', text)
+        escaped = re.sub('[<]', '&lt;', escaped)
+        escaped = re.sub('[>]', '&gt;', escaped)
+        escaped = re.sub('[%]', '&#37;', escaped)
+        escaped = re.sub('\xA0', '&nbsp;', escaped)
+        self.contents.append(escaped)
+        return self
+
+    def append_new_tag(self, *args, **kwargs):
+        """Create a new tag and add it to the contents. Returns the new tag."""
+        new_tag = Tag(*args, **kwargs)
+        self.contents.append(new_tag)
+        return new_tag
+
+    def get_attribs_str(self):
+        """Return the attributes as a string containing
+        key1="value1" key2="value2"
+        etc.
+        """
+        astr = ""
+        attribs_list = []
+        for k,v in self.attribs.iteritems():
+            attribs_list.append('%s="%s"' % (k, v))
+        if attribs_list:
+            astr += " " + (" ".join(attribs_list))
+        return astr
+
+    def get_opening_tag(self):
+        """The string for the opening tag"""
+        return "<%s%s>" % (self.tag_name, self.get_attribs_str())
+
+    def get_closing_tag(self):
+        """The string for the closing tag"""
+        return "</%s>" % self.tag_name
+
+    def get_empty_tag(self):
+        """The tag to use in case contents are empty"""
+        return "<%s%s/>" % (self.tag_name, self.get_attribs_str())
+
+    def set(self, attrib, newval):
+        """Set an attribute. Returns self for easy chaining."""
+        self.attribs[attrib] = newval
+        return self
+
+    def get(self, attrib):
+        """Get an attribute."""
+        return self.attribs.get(attrib)
+
+    def __str__(self):
+        if not self.contents:
+            return self.get_empty_tag()
+        elif len(self.contents) == 1 and not isinstance(self.contents[0], Tag):
+            return self.get_opening_tag() + str(self.contents[0]) + self.get_closing_tag()
+        else:
+            return "%s\n%s\n%s" % (self.get_opening_tag(), "\n".join((indented(str(x), 2) for x in self.contents)), self.get_closing_tag())
+        
+def SubTag(parent, *args, **kwargs):
+    """Factory function. A convenient alias for Tag.append_new_tag -- compare
+    to ElementTree.SubElement()"""
+    return parent.append_new_tag(*args, **kwargs)
+
+
+class Html(object):
+    def __init__(self, page_title=None, css_inline=None, css_link=None):
+        self.page_title = page_title
+        self.css_inline = css_inline
+        self.css_link = css_link
+
+        self.html = Tag("html", lang="en")
+        self.head = SubTag(self.html, "head")
+        if self.page_title:
+            self.head.append_new_tag("title").append(self.page_title)
+        if self.css_link:
+            self.head.append_new_tag("link", rel="stylesheet", type_="text/css", href=self.css_link)
+        if self.css_inline:
+            self.head.append_new_tag("style", type_='text/css').append(self.css_inline)
+        self.body = SubTag(self.html, "body")
+
+    def __str__(self):
+        return "<!DOCTYPE html>\n" + str(self.html)
+
+
---
+++ reporting/vmu-reporter.py
@@ -0,0 +1,160 @@
+#!/usr/bin/python
+
+import re
+import os
+import sys
+import itertools
+import yaml
+import taglib
+from taglib import Tag, SubTag
+
+cwd = os.getcwd()
+CSS_URI = "file://%s/vmu.css" % cwd
+homepage = "file://%s/results.html" % cwd
+pkg_organized_homepage = "file://%s/packages.html" % cwd
+
+os_flavors = [('CentOS','CentOS'),
+              ('Red Hat Enterprise Linux Server','Red Hat'),
+              ('Scientific Linux','Sci Linux')]
+
+repos = [('3.1; osg','3.1 Release'),
+         ('3.1; osg-testing','3.1 Testing'),
+         ('3.1; osg > osg-testing','3.1 Release -> 3.1 Testing'),
+         ('3.2; osg','3.2 Release'),
+         ('3.2; osg-testing','3.2 Testing'),
+         ('3.2; osg > osg-testing','3.2 Release -> 3.2 Testing'),
+         ('3.1; osg > 3.2/osg','3.1 Release -> 3.2 Release'),
+         ('3.1; osg-testing > 3.2/osg-testing','3.1 Testing -> 3.2 Testing')]
+
+packages = [('osg-tested-internal','Everything'),
+            ('condor.x86_64, osg-ce-condor, rsv','Condor'),
+            ('osg-gridftp, edg-mkgridmap, rsv','GridFTP'),
+            ('osg-se-bestman, rsv','BeStMan'),
+            ('osg-voms, rsv','VOMS'),
+            ('osg-gums, rsv','GUMS')]
+
+def create_html_and_table():
+    html = taglib.Html(page_title='OSG VMU Automated Test Results', css_link=CSS_URI)
+    html.body.append_new_tag('h1').append('OSG VMU Automated Test Results')
+    table = html.body.append_new_tag('table', class_ = 'padded')
+
+    return html, table
+
+def make_data_grid(runs, organization_scheme='repos'):
+    organized_runs = [[None] * 6 for i in range(0,48)]
+    for run in runs:
+        # Find y value in the grid
+        for idx, flavor in enumerate(os_flavors):
+            if re.match(flavor[0], run['os_release']):
+                column = idx
+            
+        if re.search('6', run['os_release']):
+            column = column + 3
+
+        # Find x value in the grid
+        for idx, package in enumerate(packages):
+            if package[0] == run['param_packages']:
+                package_idx = idx
+
+        for idx, repo in enumerate(repos):
+            if repo[0] == run['param_sources']:
+                repo_idx = idx
+
+        if organization_scheme == 'packages':
+            row = package_idx + repo_idx  * len(packages)
+        else:
+            row = repo_idx + package_idx * len(repos)
+
+        organized_runs[row][column] = run
+    return organized_runs
+
+def populate_table(table, data, organization_scheme='repos'):
+    if organization_scheme == 'packages':
+        sort_link = homepage
+        sort_link_text = 'Sort by Release &#9660;'
+        first_column_text = 'Parent Package &#9660;'
+        second_column_text = 'OSG Release/Upgrade'
+        outer_org = packages
+        inner_org = repos
+    else:
+        sort_link = pkg_organized_homepage
+        sort_link_text = 'Sort by Package &#9660;'
+        first_column_text = 'OSG Release/Upgrade &#9660;'
+        second_column_text = 'Parent Package'
+        outer_org = repos
+        inner_org = packages
+
+    # Create header
+    header = Tag("thead")
+    row_1 = header.append_new_tag("tr")
+    th = row_1.append_new_tag("th", rowspan="2", id = 'sort')
+    th.append_new_tag('span', id = 'hide_text').append(first_column_text)
+    th.append_new_tag('a', href=sort_link, id = 'sort_link').\
+        append(sort_link_text) # mouseover text/link
+    row_1.append_new_tag("th", rowspan="2").append(second_column_text)
+    row_1.append_new_tag("th", colspan="3").append("EL5")
+    row_1.append_new_tag("th", colspan="3").append("EL6")
+
+    row_2 = header.append_new_tag("tr")
+    for os in os_flavors * 2:
+        row_2.append_new_tag("th").append(os[1])
+
+    # Populate table rows
+    table.append(header)
+    tbody = table.append_new_tag('tbody')
+
+    for row_tuple, inner in itertools.izip(enumerate(data), itertools.cycle(inner_org)):
+        idx = row_tuple[0]
+        row = row_tuple[1]
+        rowspan_size = len(inner_org)
+        if idx % sizeof_something == 0:
+            tbody.append_new_tag('tr').append_new_tag('td', class_ = 'divider')
+            trow = tbody.append_new_tag('tr')
+            trow.append_new_tag('th', rowspan=rowspan_size).append(outer_org[idx/rowspan_size][1])
+        else:
+            trow = tbody.append_new_tag('tr')
+        trow.append_new_tag('th').append(inner[1])
+        for column in row:
+            trow.append(get_test_numbers(column))
+
+def get_test_numbers(run):
+    if run['run_status'] == 0:
+        success = run['tests_ok']
+        skip = run['tests_ok_skip']
+        fail =  run['tests_failed'] + run['tests_error'] + run['tests_bad_skip']
+        if fail == 0:
+            return Tag('td', class_ = 'result pass', align='center').append("%s %s %s" % (success, skip, fail))
+        else:
+            return Tag('td', class_ = 'result fail', align='center').append("%s %s %s" % (success, skip, fail))
+    else:
+        return Tag('td', class_ = 'result die', align='center').append('DIED')
+    
+def print_html_to_file(html, filename):
+    f = open(filename, 'w')
+    f.write(str(html))
+    f.close()
+    
+    
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print "usage: %s <analyze-test-run file>" % sys.argv[1]
+        sys.exit(1)
+
+    # setup html objects
+    html_repo, table_repo = create_html_and_table()
+    html_package, table_package = create_html_and_table()
+    
+    # Map runs to their rows
+    filename = sys.argv[1]
+    f = open(filename, 'r')
+    yaml_data = f.read()
+    f.close()
+    runs = yaml.load(yaml_data)
+
+    repo_organized_runs = make_data_grid(runs)
+    populate_table(table_repo, repo_organized_runs)
+    print_html_to_file(html_repo, 'results.html')
+    
+    pkg_organized_runs = make_data_grid(runs, organization_scheme='packages')
+    populate_table(table_package, pkg_organized_runs, organization_scheme='packages')
+    print_html_to_file(html_package, 'packages.html')
