96177927649bd1f22ca51279e5374692ea97143e
Brian Lin <blin@cs.wisc.edu>
2015-04-21 18:59:27 +0000

    Add ability to specify multiple different sets of parameters by specifying yaml files in the parameters.d folder
    git-svn-id: https://vdt.cs.wisc.edu/svn/new-test/trunk/vm-test-runs@20913 4e558342-562e-0410-864c-e07659590f8c

--- vmu.py
+++ vmu.py
@@ -2,13 +2,56 @@
 
 '''A collection of functions that are used across the various VMU automated test scripts'''
 
+import os
 import re
+import sys
+import yaml
+
+def die(message, code=1):
+    '''Write message to STDERR and exit with code'''
+    sys.stderr.write(message + '\n')
+    sys.exit(code)
+
+def write_file(contents, file_path):
+    '''Write contents to file_path'''
+    f = open(file_path, 'w')
+    f.write(contents)
+    f.close()
+
+def load_run_params(param_dir):
+    '''Read all yaml parameter files in a directory and add them to the list'''
+    run_params = []
+    for param_file in os.listdir(param_dir):
+        param_path = "%s/%s" % (param_dir, param_file)
+        yaml_file = open(param_path)
+        yaml_contents = yaml_file.read()
+        yaml_file.close()
+        try:
+            param_contents = yaml.load(yaml_contents)
+        except (yaml.scanner.ScannerError, yaml.reader.ReaderError):
+            # skip non-yaml files
+            pass
+        else:
+            if param_contents.has_key('platform') \
+               and param_contents.has_key('sources') \
+               and param_contents.has_key('packages'):
+                run_params.append(param_contents)
+    if not run_params:
+        die("Could not find parameter files in parameter directory '%s'" % param_dir)
+    return run_params
 
 def canonical_os_string(os_release):
-    '''Make the OS release from /etc/redhat-release human readable'''
+    '''Make the OS release from test parameters or /etc/redhat/release human readable'''
+    # Handle OS string from /etc/redhat-release
     result = os_release.replace('Red Hat Enterprise Linux Server', 'RHEL')
     result = result.replace('Scientific Linux', 'SL')
-    result = re.sub(r'(\d)\.\d+', '\\1', result)
+    result = result.replace('CentOS Linux', 'CentOS')
+    result = re.sub(r'(\d)\.\d+.*', r'\1', result)
+    # Handle OS string from 'platform' test parameters
+    result = result.replace('rhel', 'RHEL')
+    result = result.replace('sl', 'SL')
+    result = result.replace('centos', 'CentOS')
+    result = re.sub(r'_(\d)_.*', r' \1', result)
     return result
 
 def canonical_pkg_string(package):
