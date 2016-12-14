0bcb9278e10bbfcac36ee8c7b63e5d3e3995f7b7
Brian Lin <brianhlin@gmail.com>
2016-12-14 12:08:25 -0600

    Add PackageSet class

--- vmu.py
+++ vmu.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-'''A collection of functions that are used across the various VMU automated test scripts'''
+'''A collection of functions that are used across the various VMU automated test
+scripts'''
 
 from glob import glob
 import re
 import sys
 import yaml
-import copy
 
 def die(message, code=1):
     '''Write message to STDERR and exit with code'''
@@ -115,3 +115,83 @@ def canonical_src_string(sources):
     result = re.sub(r',', ' + ', result)
     result = re.sub(r'^(\d+\.\d+)(.*-> )(?!\d)', '\\1\\2\\1 ', result) # Duplicate release series, when needed
     return result.strip()
+
+class ParamError(Exception):
+    """Exception for errors in parameter files"""
+    pass
+
+class PackageSet(object):
+    """A class for comparing package sets as specified in VMU test parameters
+
+    Instance parameters:
+
+    label: string describing the package set, used for reporting
+    packages: list of packages as strings
+    selinux: boolean to turn on/off SELinux enforcing mode (default: False)
+    java: boolean to pre-install OSG java packages (default: True)
+
+    PackageSets are equal if their 'packages' lists are the same. If 'packages'
+    lists are equal but 'label' is not, ParamError is raised.
+
+    PackageSets are sorted by labels according to LABEL_ORDER. Unknown labels
+    are sorted alphanumerically after the known labels.
+    """
+
+    OSG_JAVA_DEFAULT = True
+    SELINUX_DEFAULT = False
+    LABEL_ORDER = ['All', 'All + GRAM', 'All + GRAM (3.2)', 'HTCondor', 'GridFTP', 'BeStMan', 'VOMS', 'GUMS']
+
+    def __init__(self, label, packages, selinux=SELINUX_DEFAULT, java=OSG_JAVA_DEFAULT):
+        if not label:
+            raise ParamError("PackageSet 'label' field cannot be blank")
+        if not packages:
+            raise ParamError("PackageSet 'packages' field must be non-empty list")
+        self.label = label
+        self.selinux = selinux
+        self.java = java
+
+        if self.java:
+            self.packages = ['java-1.7.0-openjdk-devel', 'osg-java7-compat', 'osg-java7-devel-compat'] + packages
+        else:
+            self.packages = packages
+
+    def __eq__(self, other):
+        if isinstance(other, PackageSet):
+            if self.packages == other.packages:
+                if self.label != other.label:
+                    raise ParamError('Different package set tags refer to the same set of packages')
+                return True
+            return False
+        return NotImplemented
+
+    def __ne__(self, other):
+        result = self.__eq__(other)
+        if result is NotImplemented:
+            return result
+        return not result
+
+    def __lt__(self, other):
+        def get_sort_val(pkg_set):
+            try:
+                return pkg_set.LABEL_ORDER.index(pkg_set.label)
+            except ValueError:
+                return pkg_set.label
+        return get_sort_val(self) < get_sort_val(other)
+
+    def __hash__(self):
+        # Treat similarly labeled sets of packages as equal for hashing;
+        # SELinux status can differ on a per-run basis
+        return hash(repr(self).replace('%s, ' % self.selinux, ''))
+
+    def __repr__(self):
+        return str([self.label, self.selinux, self.packages])
+
+    @classmethod
+    def from_dict(cls, pkg_set_dict):
+        '''Create a PackageSet object from a dictionary, setting selinux and
+        java attributes to SELINUX_DEFAULT and OSG_JAVA_DEFAULT, respectively,
+        if they're not defined'''
+        return cls(pkg_set_dict['label'],
+                   pkg_set_dict['packages'],
+                   pkg_set_dict.get('selinux', cls.SELINUX_DEFAULT),
+                   pkg_set_dict.get('osg_java', cls.OSG_JAVA_DEFAULT))
