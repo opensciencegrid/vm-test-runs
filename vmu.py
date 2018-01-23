8b94fce525f2384ad61253875241a6f3563c823a
Carl Edquist <edquist@cs.wisc.edu>
2018-01-22 19:32:09 -0600

    ... and, while I'm here ...
    I'd think if this section were actually parsed into tokens, the
    mapping to friendly-names would be easier, but since re.sub()'s are
    happening, substrings like osg-upcoming have to come after longer
    names that include them, in order for the longer ones to ever work...

--- vmu.py
+++ vmu.py
@@ -85,8 +85,8 @@ def canonical_src_string(sources):
     result = re.sub(r'osg-development', 'Development', result)
     result = re.sub(r'osg-testing', 'Testing', result)
     result = re.sub(r'osg-prerelease', 'Prerelease', result)
-    result = re.sub(r'osg-upcoming', 'Upcoming', result)
     result = re.sub(r'osg-upcoming-testing', 'Upcoming Testing', result)
+    result = re.sub(r'osg-upcoming', 'Upcoming', result)
     result = re.sub(r'osg', 'Release', result) # Must come after other repos
     result = re.sub(r';', '', result)
     result = re.sub(r'/', ' ', result)
